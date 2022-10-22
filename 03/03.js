2ea31d17e0f59d57f0f1c0e7110c4fad82cc9a5e
Elodie mongellaz <mongellaz.e@gmail.com>
2022-10-22 10:02:46 +0200

    correction in js

--- 03/03.js
+++ 03/03.js
@@ -1,17 +1,17 @@
 'use strict';
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 6; i += 1) {
   let row = '';
-  if (i % 2 == 0) {
-    for (let j = 0; j < 7; j++) {
-      if (j % 2 == 0) {
+  if (i % 2 === 0) {
+    for (let j = 0; j < 7; j += 1) {
+      if (j % 2 === 0) {
         row += '_';
       } else {
         row += '#';
       }
     }
   } else {
-    for (let j = 0; j < 7; j++) {
-      if (j % 2 == 0) {
+    for (let j = 0; j < 7; j += 1) {
+      if (j % 2 === 0) {
         row += '#';
       } else {
         row += '_';
